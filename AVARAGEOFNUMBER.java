2e6a1434fa2b1d03b924abac94e7e6dded2515d6
AshanMihiranga <85435511+AshanMihiranga@users.noreply.github.com>
2021-06-20 15:52:35 +0530

    Add files via upload

---
+++ AVARAGEOFNUMBER.java
@@ -0,0 +1,8 @@
+class AVARAGEOFNUMBER{
+	public static void main(String[] args){
+	int num1=1,num2=2,num3=3,num4=4,num5=5,sum,avarage;
+	sum=num1+num2+num3+num4+num5;
+	avarage=sum/5;
+	System.out.println(avarage);
+	}
+}
