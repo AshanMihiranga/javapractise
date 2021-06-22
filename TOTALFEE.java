f7ea6818fac76abf7225a9c33c588ba42138cd9e
AshanMihiranga <85435511+AshanMihiranga@users.noreply.github.com>
2021-06-22 15:51:46 +0530

    Add files via upload

---
+++ TOTALFEE.java
@@ -0,0 +1,11 @@
+class TOTALFEE{
+	public static void main(String [] args){
+	/*
+	X=number of pen
+	Y=price of one pen
+	*/
+	int X=5,Y=10,TotalFeeOfPen;
+	TotalFeeOfPen=X*Y;
+	System.out.println(TotalFeeOfPen);
+	}
+}
