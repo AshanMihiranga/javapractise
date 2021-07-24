3adcbbf0cc0a93b9ae6ff1eb90398208bc2e86e8
AshanMihiranga <85435511+AshanMihiranga@users.noreply.github.com>
2021-07-24 14:32:23 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,9 @@
+class Test{
+public void meth1(){
+System.out.println("m1 is the seconed method other than main method");
+}
+public static void main(String[] args){
+Test x=new Test();
+x.meth1();
+}
+}
