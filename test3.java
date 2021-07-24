864a5c910167e40c70b999a799e1f06883a9fe45
AshanMihiranga <85435511+AshanMihiranga@users.noreply.github.com>
2021-07-24 14:32:57 +0530

    Add files via upload

---
+++ test3.java
@@ -0,0 +1,33 @@
+class test3{
+public void meth1(){
+System.out.println("Science");
+}
+public void meth2(){
+System.out.println("Maths");
+}
+public void meth3(){
+System.out.println("English");
+}
+public void meth4(){
+System.out.println("Sinhala");
+}
+public void meth5(){
+System.out.println("Tamil");
+}
+public void meth6(){
+System.out.println("History");
+}
+public static void main(String[] args){
+test3 x=new test3();
+x.meth1();
+x.meth2();
+x.meth3();
+x.meth4();
+x.meth5();
+x.meth6();
+x.meth7();
+}
+public void meth7(){
+System.out.println("IT");
+}
+}
