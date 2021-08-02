e17da5a2d9095d956e8589cbd2f6f33c8d82bcdb
AshanMihiranga <85435511+AshanMihiranga@users.noreply.github.com>
2021-08-02 14:57:37 +0530

    Add files via upload

---
+++ T2.java
@@ -0,0 +1,25 @@
+class T2{
+public void meth1(){
+System.out.println("i am X");
+}
+public void meth2(){
+System.out.println("i live in Nugegoda");
+}
+public void meth3(){
+System.out.println("i have one brother");
+}
+public void meth4(){
+System.out.println("I got a pet parrot");
+}
+public void meth5(){
+System.out.println("i go to university");
+}
+public static void main(String[] args){
+T2 x = new T2();
+x.meth1();
+x.meth2();
+x.meth3();
+x.meth4();
+x.meth5();
+}
+}
