71806ef2c4bf9dcfcb68e4cda46d8ea328ab0d69
AshanMihiranga <85435511+AshanMihiranga@users.noreply.github.com>
2021-08-02 14:57:16 +0530

    Add files via upload

---
+++ T1.java
@@ -0,0 +1,9 @@
+class T1{
+public static void main (String[]args){
+System.out.println("i am X");
+System.out.println("i live in Nugegoda");
+System.out.println("i have one brother");
+System.out.println("i got a pet parrot");
+System.out.println("i go to university");
+}
+}
